1a9a937d83179d4bf15f82a7502e6facffc86bc1
Kenneth-Kafunya <kennethkafunya@gmail.com>
2026-02-26 10:33:24 +0200

    favicon, hero padding

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,10 @@ import { SocialIcons } from "./IconMap";
 export default function Footer() {
   const year = new Date().getFullYear();
   return (
-    <footer className="bg-gray-800 text-gray-300 p-8 text-center [@media(min-width:768px)_and_(max-width:1024px)]:p-6
-">
+    <footer
+      className="bg-gray-800 text-gray-300 p-8 text-center [@media(min-width:768px)_and_(max-width:1024px)]:p-6
+"
+    >
       <div className="footerContainer flex justify-between font-light text-sm max-sm:flex-col max-sm:gap-4 max-sm:items-center [@media(min-width:768px)_and_(max-width:1024px)]:flex-col [@media(min-width:768px)_and_(max-width:1024px)]:items-center [@media(min-width:768px)_and_(max-width:1024px)]:gap-4">
         {" "}
         <p>&copy;{`${year}`} Kenneth Kafunya, All rights reserved.</p>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,37 +3,38 @@ import Navbar from "./Navbar";
 
 export default function Hero() {
   return (
-    <section
-      id="home"
-      className="h-dvh flex items-center justify-center px-6
+    <>
+      <Navbar />
+      <section
+        id="home"
+        className="h-dvh flex items-center justify-center px-6
 [@media(min-width:768px)_and_(max-width:1024px)]:min-h-dvh
 "
-    >
-      <Navbar />
+      >
+        <div className="flex flex-col w-1/2 mx-auto mt-11 max-sm:w-full max-sm:px-6 max-md:text-center max-xl:py-11 ">
+          <h1 className="text-green-900 text-8xl max-md:text-6xl max-sm:text-6xl font-bold -tracking-normal">
+            Hey, Kenneth here —{" "}
+          </h1>
+          <div className="descriptionHeader text-green-900 max-sm:pt-3 max-sm:w-1/2 flex mx-auto py-auto max-sm:justify-center max-sm:flex-col max-sm:gap-3">
+            <span className="text-6xl max-lg:text-4xl max-sm:text-xl tracking-tight pt-1">
+              <span className="">Product designer</span> and{" "}
+              <span className="">Coding beginner</span>.{" "}
+            </span>
+          </div>
 
-      <div className="flex flex-col w-1/2 max-sm:w-full max-sm:px-6 max-md:text-center max-xl:py-11 ">
-        <h1 className="text-green-900 text-8xl max-md:text-6xl max-sm:text-6xl font-bold -tracking-normal">
-          Hey, Kenneth here —{" "}
-        </h1>
-        <div className="descriptionHeader text-green-900 max-sm:pt-3 max-sm:w-1/2 flex mx-auto py-auto max-sm:justify-center max-sm:flex-col max-sm:gap-3">
-          <span className="text-6xl max-lg:text-4xl max-sm:text-xl tracking-tight pt-1">
-            <span className="">Product designer</span> and{" "}
-            <span className="">Coding beginner</span>.{" "}
-          </span>
+          <p className="subText text-gray-500 text-xl max-sm:text-[1rem] pt-2">
+            For founders, startups and next-gen Brands.
+          </p>
+          <div className="button flex gap-6 mt-11 flex-row max-md:items-center max-md:flex-col">
+            <LargeBtn title={"Download CV"} />
+            <LargeBtn
+              title={"Let’s get to work"}
+              BtnStyle={true}
+              emailBtn={true}
+            />
+          </div>
         </div>
-
-        <p className="subText text-gray-500 text-xl max-sm:text-[1rem] pt-2">
-          For founders, startups and next-gen Brands.
-        </p>
-        <div className="button flex gap-6 mt-11 flex-row max-md:items-center max-md:flex-col">
-          <LargeBtn title={"Download CV"} />
-          <LargeBtn
-            title={"Let’s get to work"}
-            BtnStyle={true}
-            emailBtn={true}
-          />
-        </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
